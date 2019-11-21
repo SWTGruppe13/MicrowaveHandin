7e23c8d7412bbdddd7aec17f7f82bf5e163a3d11
FranzVDB <Franz96ftw@hotmail.com>
2019-11-21 15:08:11 +0100

    Fixed things in PowerTube with power being % changed it to W and fixed tests. + finished tests for step 2

--- Microwave.Test.Integration/IntegrationTestSteps/IT2_CC_PT.cs
+++ Microwave.Test.Integration/IntegrationTestSteps/IT2_CC_PT.cs
@@ -18,7 +18,6 @@ namespace Microwave.Test.Integration.IntegrationTestSteps
         private IPowerTube _powerTube;
         private IOutput _fakeOutput;
 
-
         [SetUp]
         public void SetUp()
         {
@@ -30,20 +29,41 @@ namespace Microwave.Test.Integration.IntegrationTestSteps
             _uut = new CookController(_fakeTimer,_display,_powerTube,_fakeUserInterface);
         }
 
-        [Test]
-        public void PowerTube_turnOn_called_output_right()
+        [TestCase(50)] // min
+        [TestCase(51)] // min +1
+        [TestCase(100)] // random
+        [TestCase(200)] // random
+        [TestCase(699)] // max -1
+        [TestCase(700)] // max
+        public void PowerTube_turnOn_called_output_right(int power)
         {
-            _uut.StartCooking(50,1);
-            _fakeOutput.Received().OutputLine($"PowerTube works with 50 %");
+            _uut.StartCooking(power,1);
+            _fakeOutput.Received().OutputLine($"PowerTube works with {power} W");
         }
 
         [Test]
-        public void PowerTube_turnOff_called_output_right() //husk fejl ved power kun kan være mellem 0 - 100 burde være 50 - 700.
+        public void PowerTube_turnOff_called_output_right() //husk fejl ved power kun kan være mellem 0 - 100 burde være 50 - 700. - Skulle være fixet nu
         {
             _uut.StartCooking(50,50);
             _uut.Stop();
             _fakeOutput.Received().OutputLine($"PowerTube turned off");
         }
 
+
+        [TestCase(49)] // min
+        [TestCase(-50)] // negative
+        [TestCase(1000)] // random
+        [TestCase(701)] // max
+        public void Throw_OutOfRange_NotAllowed(int power)
+        {
+            Assert.That(() => _uut.StartCooking(power,50), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void Throw_IsAllReadyOn_NotAllowed()
+        {
+            _uut.StartCooking(50,60);
+            Assert.That(() => _uut.StartCooking(50, 50), Throws.TypeOf<ApplicationException>());
+        }
     }
 }
--- MicrowaveOvenClasses/Boundary/PowerTube.cs
+++ MicrowaveOvenClasses/Boundary/PowerTube.cs
@@ -18,7 +18,7 @@ namespace MicrowaveOvenClasses.Boundary
         {
             if (power < 50 || 700 < power)
             {
-                throw new ArgumentOutOfRangeException("power", power, "Must be between 1 and 100 % (incl.)");
+                throw new ArgumentOutOfRangeException("power", power, "Must be between 50 and 700 W (incl.)");
             }
 
             if (IsOn)
@@ -26,7 +26,7 @@ namespace MicrowaveOvenClasses.Boundary
                 throw new ApplicationException("PowerTube.TurnOn: is already on");
             }
 
-            myOutput.OutputLine($"PowerTube works with {power} %");
+            myOutput.OutputLine($"PowerTube works with {power} W");
             IsOn = true;
         }
 
