8b8fa4986450ad2f27cf95abe4be198c272077b6
FranzVDB <Franz96ftw@hotmail.com>
2019-11-26 15:53:37 +0100

    Tests for step nr 5 er lavet

--- MicrowaveOvenClasses/Boundary/PowerTube.cs
+++ MicrowaveOvenClasses/Boundary/PowerTube.cs
@@ -16,9 +16,9 @@ namespace MicrowaveOvenClasses.Boundary
 
         public void TurnOn(int power)
         {
-            if (power < 50 || 700 < power)
+            if (power < 50 || 700 < power) // changed from 0 - 100 % to 50 - 700 W
             {
-                throw new ArgumentOutOfRangeException("power", power, "Must be between 50 and 700 W (incl.)");
+                throw new ArgumentOutOfRangeException("power", power, "Must be between 50 and 700 W (incl.)"); // changed % to W
             }
 
             if (IsOn)
